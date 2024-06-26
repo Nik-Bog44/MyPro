f0827fd44d511bca32262dda16e004b99598463b
nikolay_bogdan <dhjan44@gmail.com>
2024-06-26 16:19:28 +0300

    Initial commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import social_core.backends.vk
 from envparse import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,16 +36,25 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     # django
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Third party app,
+    'rest_framework',
+    'django_filters',
+    'social_django',
     # First party app,
-    "todolist.core",
+    'todolist.core',
+    'todolist.goals',
+    'todolist.bot',
 ]
+if DEBUG:
+    INSTALLED_APPS += [
+        'django_extensions',
+    ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -130,3 +140,27 @@ STATIC_ROOT = BASE_DIR.joinpath('static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# vk
+# SOCIAL_AUTH_JSONFIELD_ENABLED = True
+# SOCIAL_AUTH_JSONFIELD_CUSTOM = 'django.db.models.JSONField'
+# SOCIAL_AUTH_VK_OAUTH2_KEY = env.str('VK_OAUTH_ID')
+# SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str('VK_OAUTH_SECRET_KEY')
+# AUTHENTICATION_BACKENDS = ('social_core.backends.vk.VKOAuth2', 'django.contrib.auth.backends.ModelBackend')
+# SOCIAL_AUTH_URL_NAMESPACE = 'social'
+# SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+# SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
+# SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+# SOCIAL_AUTH_VK_EXTRA_DATA = [('email', 'email')]
+# SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/logged-in/'
+# SOCIAL_AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+}
+
+BOT_TOKEN = env.str('BOT_TOKEN')
+
+
+
